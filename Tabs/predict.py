9fc606b47665e54e5acf689e21bb45783f0329c7
Aaron Sequeira <96731649+aaronseq12@users.noreply.github.com>
2023-10-05 20:47:30 +0300

    Update predict.py

--- Tabs/predict.py
+++ Tabs/predict.py
@@ -1,7 +1,7 @@
 """This modules contains data about prediction page"""
 
 # Import necessary modules
-import streamlit as st
+import streamlit as s
 
 # Import necessary functions from web_functions
 from web_functions import predict
